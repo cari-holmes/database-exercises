9d6afd080ff653e2e0b57a4b2b0debfe935a3aed
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-03 14:29:19 -0500

    mysql intro

--- 3.1_mysql_introduction.sql
+++ 3.1_mysql_introduction.sql
@@ -1,9 +1,13 @@
 show databases;
 
+-- shows al databases
 select * from mysql.user;
 
+--mysql.user means use the mysql database and read from the user table
 select user, host from mysql.user;
 
+--select all columns from the help_topic table on the mysql database
 select * from mysql.help_topic;
 
+--select only the help_topic_id, help_category_id, and url columns
 select help_topic_id, help_category_id, url from mysql.help_topic;
