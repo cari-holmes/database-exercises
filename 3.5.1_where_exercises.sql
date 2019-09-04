d4116971f4a4d70b4c9a45945623eda649f04eb7
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-04 10:33:41 -0500

    changed hire date by one

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -6,7 +6,7 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT * FROM employees WHERE last_name LIKE 'e%';
 
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 
