d06147fa9028749add880f689ac5b44dc6e30f27
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-04 10:27:24 -0500

    reviewed

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -22,4 +22,4 @@ SELECT * FROM employees WHERE last_name LIKE 'e%e';
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '[!qu]%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
