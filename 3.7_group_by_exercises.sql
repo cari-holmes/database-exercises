45c695dcc38ff10ba0e35fce9f2dace66aaa3519
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-04 16:02:29 -0500

    group by exercises

---
+++ 3.7_group_by_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
