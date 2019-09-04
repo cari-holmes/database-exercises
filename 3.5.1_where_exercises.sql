ee563ef864b04e618cf3c9b74d6008cca2df0acf
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-04 10:25:42 -0500

    reviewed

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -16,11 +16,10 @@ SELECT * FROM employees WHERE first_name = 'Irena' OR first_name='Vidya' OR firs
 
 SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name='Vidya' OR first_name= 'Maya') AND gender='M';
 
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR '%e';
---
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 
 SELECT * FROM employees WHERE last_name LIKE 'e%e';
 
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT LIKE '[!qu]%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '[!qu]%';
