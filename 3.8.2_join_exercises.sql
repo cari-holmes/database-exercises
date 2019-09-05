53c2be29b572dc26bd996ee8d50661333d7117bd
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-05 16:40:50 -0500

    more join excercises

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -1,3 +1,7 @@
+
+SELECT * FROM users;
+SELECT * FROM roles;
+
 SELECT users.name AS user_name, roles.name AS role_name FROM users
 JOIN roles ON users.role_id=roles.id;
 
@@ -7,6 +11,11 @@ LEFT JOIN roles ON users.role_id = roles.id;
 SELECT users.name AS usr_name, roles.name AS role_name FROM users
 RIGHT JOIN roles ON users.role_id = roles.id;
 
+SELECT u.*, r.*
+FROM users AS u
+RIGHT JOIN roles as r;
+
+--
 
 USE employees;
  
@@ -20,8 +29,9 @@ ON dm.emp_no = e.emp_no
 JOIN departments AS d 
 ON d.dept_no = dm.dept_no
 
-WHERE dm.to_date = '9999-01-01';
+WHERE dm.to_date = '9999-01-01' ORDER BY d.dept_name;
 
+-- 
 
 SELECT d.dept_name AS department_name, CONCAT(e.first_name, ' ', e.last_name) AS department_manager 
 FROM employees AS e
@@ -32,8 +42,9 @@ ON dm.emp_no = e.emp_no
 JOIN departments AS d 
 ON d.dept_no = dm.dept_no
 
-WHERE dm.to_date = '9999-01-01' AND gender = 'F';
+WHERE dm.to_date = '9999-01-01' AND gender = 'F' ORDER BY d.dept_name;
 
+--
 
 SELECT title, COUNT(*) FROM titles
 
@@ -46,6 +57,7 @@ ON d.dept_no = de.dept_no
 WHERE de.to_date = "9999-01-01" AND titles.to_date = "9999-01-01" AND dept_name = "customer service"
 GROUP BY title;
 
+-- 
 
 SELECT dept_name AS department_name, CONCAT(e.first_name, ' ', e.last_name) AS name, s.salary AS salary
 FROM dept_manager AS dm
@@ -62,8 +74,29 @@ ON d.dept_no = dm.dept_no
 WHERE dm.to_date = "9999-01-01" AND s.to_date = "9999-01-01" ORDER BY dept_name ASC
 ;
 
-SELECT dept_no, d.dept_name, COUNT(employees) AS num_employees 
+--
+
+SELECT de.dept_no, d.dept_name AS dept_name, COUNT(*) AS num_employees
 FROM dept_emp AS de
 
+JOIN departments AS d
+ON d.dept_no = de.dept_no
 
+JOIN employees AS e
+ON e.emp_no = de.emp_no
 
+WHERE de.to_date = '9999-01-01' 
+GROUP BY de.dept_no, d.dept_name;
+
+--
+
+SELECT d.dept_name AS dept_name, AVG(s.salary) AS average_salary
+FROM dept_emp AS de
+
+JOIN departments AS d
+ON d.dept_no = de.dept_no
+
+JOIN salaries AS s
+ON s.emp_no = de.emp_no
+
+ORDER BY d.dept_name DESC;
