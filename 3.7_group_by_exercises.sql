51dc3e94341292d43f6480dfda23e508a1a111d6
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-05 09:12:02 -0500

    finished exercises

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -9,8 +9,21 @@ SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%e';
 
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
---
+SELECT DISTINCT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY(last_name);
 
 SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
 
---
+SELECT
+	LOWER(CONCAT(
+			SUBSTR(first_name, 1, 1),
+			SUBSTR(last_name, 1, 4),
+			'_',
+			SUBSTR(birth_date, 6, 2),
+			SUBSTR(birth_date, 3, 2)
+		) 
+	) AS user_name, COUNT(*)
+	
+	
+FROM employees
+GROUP BY user_name
+ORDER BY COUNT(user_name) DESC;
