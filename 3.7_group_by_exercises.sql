8a054e3da39b01dba3a4232388d381142e3c6a81
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-05 09:40:05 -0500

    reviewed exercises

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -2,14 +2,14 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'e%e'
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e'
 GROUP BY last_name;
 
-SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%e';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name, first_name;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
-SELECT DISTINCT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY(last_name);
+SELECT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY(last_name);
 
 SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
 
@@ -22,8 +22,7 @@ SELECT
 			SUBSTR(birth_date, 3, 2)
 		) 
 	) AS user_name, COUNT(*)
-	
-	
 FROM employees
 GROUP BY user_name
-ORDER BY COUNT(user_name) DESC;
+ORDER BY COUNT(*) DESC;
+
