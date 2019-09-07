6a5ba50802541e2f34872f2285545762914c4e94
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-07 18:08:11 -0500

    SQL Subquery Exercises

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -1,16 +1,18 @@
+-- Objective: Understand how to use subqueries and the difference between a subquery and JOIN
+
 USE employees;
 
--- 1
+-- 1. Find all employees with the same hire date as employee 101010 using a subquery
 SELECT first_name, last_name, hire_date FROM employees
 WHERE hire_date IN (
 	SELECT hire_date 
 	FROM employees
 	WHERE emp_no='101010');
-	
+
 SELECT * FROM employees
 WHERE emp_no = '101010';
 
--- 2
+-- 2 Find all the titles held by all employees with the first name Aamod
 SELECT title, COUNT(title) FROM titles
 WHERE emp_no IN (
 	SELECT emp_no 
@@ -18,14 +20,15 @@ WHERE emp_no IN (
 	WHERE first_name="AAmod")
 	GROUP BY title;
 	
--- 3
+-- 3. How many people in the employees table are no longer working for the company?
 SELECT COUNT(emp_no) FROM employees
 WHERE emp_no IN (
 	SELECT emp_no
 	FROM dept_emp
 	WHERE NOT to_date = "9999-01-01"
 	GROUP BY emp_no);
-	
+
+-- -- --	
 SELECT COUNT(*)
 FROM employees
 WHERE emp_no IN (
@@ -34,14 +37,14 @@ FROM dept_emp
 GROUP BY emp_no
 HAVING MAX(to_date)<NOW());
 	
--- 4
+-- 4. Find all the current department managers that are female
 SELECT first_name, last_name FROM employees 
 WHERE gender = "F" AND emp_no IN (
 	SELECT emp_no
 	FROM dept_manager
 	WHERE to_date="9999-01-01");
 	
--- 5
+-- 5. Find all the employees that currently have a higher than average salary
 SELECT AVG(salary) FROM salaries;
 
 SELECT first_name, last_name, salary
@@ -55,7 +58,7 @@ WHERE to_date="9999-01-01" AND salary > (
 	FROM salaries
 	);
 	
--- 6
+-- 6. How many current salaries are within one standard deviation of the hightest salary?
 SELECT MAX(salary) FROM salaries;
 
 SELECT COUNT(*), COUNT(*)/(SELECT COUNT(*) * 100 WHERE to_date>NOW()) FROM salaries 
