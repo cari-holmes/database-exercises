d12cbb21ab418b863247562ad1c7ddb8c8d31703
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-07 17:07:05 -0500

    SQL Limit Exercises

--- 3.5.3_limit_exercises.sql
+++ 3.5.3_limit_exercises.sql
@@ -1,10 +1,15 @@
+-- Objective: Understand how to get query results in batches using LIMIT
+
+-- 1. Use employees
 USE employees;
 
--- LIMIT Exercises
+-- 2. Find all the unique last names within the company, limiting to 10
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
+-- 3. Update previous query to find just the first 5 employees
+SELECT first_name, last_name FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
 
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
-
--- (Offset / limit) + 1 = page number, (45 / 5) + 1 = 10 given that we were told page 10, 10 -1 = 9 * 5(limit) = 45(offset)
+-- 4. Update the query to find the 'tenth page' of results
+SELECT first_name, last_name FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01' AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
+-- What is the relationship between OFFSET, LIMIT and the page number?
+-- (OFFSET / LIMIT) + 1 = page number, (45 / 5) + 1 = 10 given that we were told page 10, 10 -1 = 9 * 5(LIMIT) = 45(OFFSET)
