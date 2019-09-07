170b7ad1fbefe5bdde91c1b5d86e890007b788be
Cari Holmes <holmes.l.cari@gmail.com>
2019-09-07 18:14:28 -0500

    SQL Temporary Tables Exercises

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -1,5 +1,8 @@
+-- Objective: Learn about temporary tables
+
 use bayes_818;
 
+-- 1. Recreate the employees_with_departments table
 CREATE TEMPORARY TABLE employees_with_departments AS
 SELECT emp_no, first_name, last_name, dept_no, dept_name
 FROM employees.employees
@@ -8,17 +11,24 @@ JOIN employees.departments USING(dept_no);
 
 SELECT * FROM employees_with_departments;
 
+-- A. Add a column to this table called full_name with a VARCHAR length the sum of the first_name and last_name columns
 ALTER TABLE employees_with_departments ADD full_name VARCHAR(30);
 
 SELECT * FROM employees_with_departments;
 
+-- B. Update the table so that the full name column contains the correct data
 UPDATE employees_with_departments 
 SET full_name = CONCAT(first_name, " ", last_name);
 
+SELECT * FROM employees_with_departments;
+
+-- C. Remove the first_name and last_name columns from the table
 ALTER TABLE employees_with_departments DROP COLUMN first_name;
 ALTER TABLE employees_with_departments DROP COLUMN last_name;
 
+-- D. What is another way to create the same table?
 
+-- 2. Create a temporary table based on the payment table from sakila database
 CREATE TEMPORARY TABLE pay AS
 SELECT * FROM sakila.payment;
 
